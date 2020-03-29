fb04828f38525b3d7ce26817882af2d01f88c409
Kasia Bratkowska <kasiabratek8@gmail.com>
2020-03-29 20:08:42 +0200

    ok

--- statki.js
+++ statki.js
@@ -1,6 +1,7 @@
-var location1=3;
-var location2=4;
-var location3=5;
+var randomLoc=Math.floor(Math.random()*5);
+var location1= randomLoc;
+var location2= location1+1;
+var location3=location2+1;
 var guess;
 var hits=0;
 var guesses=0;
@@ -10,7 +11,7 @@ while (isSunk==false)
     guess= prompt("Gotów cel pal! (podaj liczbę z zakresu od 0-6): ");
 if( guess<0 || guess>6){
     alert("Proszę podać prawidłowy numer komórki");
-}
+    }
 else{
     guesses=guesses+1;
 
